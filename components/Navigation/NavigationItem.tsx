8bc83c134c3ac6730e5c931eca16477c3a0eb92f
herisusantoarismann <heri.s.arisman@gmail.com>
2022-08-09 12:10:33 +0700

    Update route active

--- components/Navigation/NavigationItem.tsx
+++ components/Navigation/NavigationItem.tsx
@@ -19,7 +19,7 @@ const NavigationItem = ({ data }: Props) => {
         <div className="w-full flex gap-2">
           <span
             className={`w-1/12 xl:w-24 h-0.5 xl:h-1.5 ${
-              pathname.includes(data.url) ? "bg-primary" : "bg-secondary"
+              data.url === pathname ? "bg-primary" : "bg-secondary"
             } `}
           ></span>
           <span className="w-5/6 xl:w-full h-0.5 xl:h-1.5 bg-gray-600"></span>
