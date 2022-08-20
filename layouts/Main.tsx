72aac62a95efa0036b9ec974d63a927aa4644cfd
herisusantoarismann <heri.s.arisman@gmail.com>
2022-08-20 09:53:42 +0700

    Update meta

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import BottomLine from "../components/BottomLine/BottomLine";
 import Content from "../components/Content/Content";
@@ -10,11 +11,23 @@ interface Props {
 
 const Main = ({ children, withArrow = false }: Props) => {
   return (
-    <div className="bg-base text-white min-h-screen flex flex-col justify-between">
-      <Header />
-      <Content>{children}</Content>
-      <BottomLine withArrow={withArrow}/>
-    </div>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Portofolio Website" />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, React, NextJS, Tailwindcss, Typescript"
+        />
+        <meta name="author" content="Heri Susanto Arisman" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className="bg-base text-white min-h-screen flex flex-col justify-between">
+        <Header />
+        <Content>{children}</Content>
+        <BottomLine withArrow={withArrow} />
+      </div>
+    </>
   );
 };
 
